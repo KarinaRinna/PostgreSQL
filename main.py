7c032449f53c5c26daf28b05a38d91fa0ec0ac0e
Mandarin <zurual@mail.ru>
2023-08-12 21:50:52 +0300

    connect test

--- main.py
+++ main.py
@@ -10,9 +10,21 @@ try:
         password=password,
         database=db_name
     )
+
+    # the cursor for performing database operations
+    #cursor = connection.cursor()
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT version();"
+        )
+
+        print(f"Server version: {cursor.fetchone()}")
+
 except Exception as _ex:
     print("[INFO] Error while working with PostgreSQL", _ex)
 finally:
     if connection:
+        #cursor.close()
         connection.close()
         print("[INFO] PostgreSQL connection closed")
